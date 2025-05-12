00f6d1f54ab06c9f3164376550bec162e05c21fc
orpaz david <orpazd7@gmail.com>
2025-05-12 19:05:26 +0300

    add class customer

---
+++ mid term exam/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mid_term_exam
+{
+    internal class Customer
+    {
+        public string id;
+        public string name;
+        public string phoneNumber;
+
+        public Customer(string id, string name, string phoneNumber)
+        {
+            this.id = id;
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"ID: {id}, Name: {name}, Phone: {phoneNumber}");
+        }
+    }
+}
