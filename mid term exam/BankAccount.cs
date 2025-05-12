a401fcb6f09b71c2857e50b6985c28b198f102f8
orpaz david <orpazd7@gmail.com>
2025-05-12 19:35:25 +0300

    add class BankAccount

---
+++ mid term exam/BankAccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mid_term_exam
+{
+    internal class BankAccount
+    {
+        protected int accountNumber;
+        protected double balance;
+        protected Customer customerOwner;
+
+        public BankAccount(int accountNumber, Customer owner)
+        {
+            this.accountNumber = accountNumber;
+            this.customerOwner = owner;
+            this.balance = 0;
+        }
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public void Deposit(double amount, string date)
+        {
+            balance += amount;
+            Console.WriteLine($"Deposited {amount} on {date}");
+        }
+
+        public virtual void Withdraw(double amount)
+        {
+            if (balance >= amount)
+            {
+                balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds.");
+            }
+        }
+
+        public virtual void PrintBalance()
+        {
+            Console.WriteLine($"Account {accountNumber} Balance: {balance}");
+        }
+
+        public int GetAccountNumber() => accountNumber;
+
+    }
+}
