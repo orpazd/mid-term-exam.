a06bb32d69f387e3144b50d9cb8f9b3f6d9b3117
orpaz david <orpazd7@gmail.com>
2025-05-12 19:03:45 +0300

    add interface

---
+++ mid term exam/IBankBranch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mid_term_exam
+{
+    internal interface IBankBranch
+    {
+        void AddAccount(BankAccount account);
+        void PrintAccounts();
+
+    }
+
+}
