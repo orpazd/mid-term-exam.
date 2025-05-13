f790607a96b7832139e9ad1fa68fb6522e965c9e
orpaz david <orpazd7@gmail.com>
2025-05-13 10:36:06 +0300

    add class CheckingAccount

---
+++ mid term exam/CheckingAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mid_term_exam
+{
+    internal class CheckingAccount : BankAccount
+    {
+        public double overdraftLimit;
+
+        public CheckingAccount(int accountNumber, Customer owner, double overdraftLimit)
+            : base(accountNumber, owner)
+        {
+            this.overdraftLimit = overdraftLimit;
+        }
+
+        public override void Withdraw(double amount)
+        {
+            if (balance + overdraftLimit >= amount)
+            {
+                balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Withdrawal denied. Exceeds overdraft limit.");
+            }
+        }
+
+        public override void PrintBalance()
+        {
+            Console.WriteLine($"Account {accountNumber} Balance: {balance}, Overdraft Limit: {overdraftLimit}");
+        }
+    }
+}
