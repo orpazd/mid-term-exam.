138e883daf0983a986a6377504105606f41a5df7
orpaz david <orpazd7@gmail.com>
2025-05-13 13:18:04 +0300

    add main

--- mid term exam/Program.cs
+++ mid term exam/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using mid_term_exam;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,18 @@ namespace Mid_term_exam
     {
         static void Main(string[] args)
         {
+            Customer cust1 = new Customer("123456789", "Idan", "050-1234567");
+            CheckingAccount acc1 = new CheckingAccount(1993, cust1, 1500);
+
+            acc1.Deposit(500);
+            acc1.Deposit(1250, "2025-05-13");
+            acc1.Withdraw(1000);
+
+            BankBranch branch = new BankBranch(1, "Netanya");
+            branch.AddAccount(acc1);
+
+            branch.PrintAccounts();
+
         }
     }
 }
